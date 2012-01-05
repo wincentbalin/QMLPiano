1877201957262564b75073b244284bce5c2cb615
Wincent Balin <wincent.balin@gmail.com>
2012-01-05 02:04:04 +0100

    Started to implement communication between core and UI

--- qmlpiano.cpp
+++ qmlpiano.cpp
@@ -2,14 +2,53 @@
 #include "qmlapplicationviewer.h"
 #include "qmlpiano.hpp"
 
+
+void Piano::decreaseOctave()
+{
+    octave--;
+    emit octaveChanged(octave);
+}
+
+void Piano::increaseOctave()
+{
+    octave++;
+    emit octaveChanged(octave);
+}
+
+void Piano::noteOn(int keyIndex)
+{
+}
+
+void Piano::noteOff(int keyIndex)
+{
+}
+
+int Piano::key2midi(int index)
+{
+    return (octave + 5) * 12 + index;
+}
+
+
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
+#if 0
     QScopedPointer<QApplication> app(createApplication(argc, argv));
 
     QmlApplicationViewer viewer;
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
     viewer.setMainQmlFile(QLatin1String("qml/QMLPiano/UI.qml"));
     viewer.showExpanded();
+#endif
 
-    return app->exec();
+    QApplication app(argc, argv);
+    QDeclarativeView view(QUrl::fromLocalFile("qml/QMLPiano/UI.qml"));
+    QObject* item = view.rootObject();
+
+    Piano piano;
+//    QObject::connect(item, SIGNAL(destroyed())
+
+    view.show();
+
+    return app.exec();
+//    return app->exec();
 }
--- qmlpiano.hpp
+++ qmlpiano.hpp
@@ -1,4 +1,41 @@
 #ifndef QMLPIANO_HPP
 #define QMLPIANO_HPP
 
+#include <QObject>
+#include <QString>
+#include <QtDeclarative>
+
+#include "RtMidi.h"
+
+// Platform-dependent sleep routines, taken from an example
+#if defined(__WINDOWS_MM__)
+  #include <windows.h>
+  #define SLEEP( milliseconds ) Sleep( (DWORD) milliseconds )
+#else // Unix variants
+  #include <unistd.h>
+  #define SLEEP( milliseconds ) usleep( (unsigned long) (milliseconds * 1000.0) )
+#endif
+
+class Piano : public QDeclarativeItem
+{
+    Q_OBJECT
+    Q_PROPERTY(int octave READ currentOctave NOTIFY octaveChanged)
+public:
+    Piano() : octave(0) {}
+    int currentOctave() const { return octave; }
+signals:
+    void octaveChanged(int octave);
+public slots:
+    void decreaseOctave();
+    void increaseOctave();
+    void noteOn(int keyIndex);
+    void noteOff(int keyIndex);
+private:
+    int key2midi(int index);
+
+    QString portName;
+    int octave;
+    RtMidiOut* midiOut;
+};
+
 #endif // QMLPIANO_HPP
